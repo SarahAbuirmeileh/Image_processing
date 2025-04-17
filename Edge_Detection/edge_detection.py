b5451e0507b4758ec458d43d53ea3e855e9df55b
Sarah Abu irmeileh <211027@ppu.edu.ps>
2025-04-17 21:37:10 +0300

    Write the convolution in the trivial way and in other way yo handel 2 kernels convolutions and test it over 2 images

--- Edge_Detection/edge_detection.py
+++ Edge_Detection/edge_detection.py
@@ -13,10 +13,10 @@ class EdgeDetection:
 
         self.gaussian_kernel = self.generate_gaussian_kernel(kernel_size=5, sigma=4.0)
         # self.gaussian_kernel = np.array([[1,4,7,4,1],
-        #                          [4,16,26,16,4],
-        #                          [7,26,41,26,7],
-        #                          [4,16,26,16,4],
-        #                          [1,4,7,4,1]], dtype=np.float32)
+            #                              [4,16,26,16,4],
+            #                              [7,26,41,26,7],
+            #                              [4,16,26,16,4],
+            #                              [1,4,7,4,1]], dtype=np.float32)
         # self.gaussian_kernel /= np.sum(self.gaussian_kernel)
 
     def _read_image(self):
@@ -25,54 +25,22 @@ class EdgeDetection:
             logging.error(f"Failed to load image from {self.image_path}.")
         return image
 
-    def convolution(self, image, kernel):
-        (image_height, image_width) = image.shape
-        (kernel_height, kernel_width) = kernel.shape
-
-        kh = kernel_height // 2
-        kw = kernel_width // 2
-
-        # Flip the kernel for true convolution
-        kernel = np.flipud(kernel)
-        kernel = np.fliplr(kernel)
-
-        output = np.zeros_like(image, dtype=np.float32)
-
-        for ix in range(kw, image_width - kw):
-            for iy in range(kh, image_height - kh):
-                # Extract the region of the image under the kernel 
-                # (extract the # of rows & columns centered at (ix, iy), extending by half the kernel size )
-                region = image[iy - kh : iy + kh + 1, ix - kw : ix + kw + 1]
-
-                output[iy, ix] = np.sum(region * kernel)
-
-        return output
-
-    def sobel_edge_detection(self, threshold):
-        sobel_image = self.convolution(self.image, self.sobel_kernel)
-        thresholded = self.threshold(sobel_image, threshold)
-        self.display_image(thresholded, "Sobel Edge Detection")
-
-    def sobel_edge_detection_with_smoothing(self, threshold):
-        smoothed_image = self.convolution(self.image, self.gaussian_kernel)
-        sobel_edge_detection = self.convolution(smoothed_image, self.sobel_kernel)
-        thresholded_image = self.threshold(sobel_edge_detection, threshold)
-        self.display_image(thresholded_image, "Smoothed Sobel Edge Detection")
-        
-        # combined_kernels = self.convolution(self.gaussian_kernel, self.sobel_kernel)
-        # sobel_edge_detection = self.convolution(self.image, combined_kernels)
-        # thresholded_image = self.threshold(sobel_edge_detection, threshold)
-        # self.display_image(thresholded_image, "Smoothed Sobel Edge Detection")
-
     def threshold(self, image, threshold: int):
+        if image is None:
+            logging.warning("No image to threshold :(")
+            return None
+
         return np.where(image >= threshold, 255, 0).astype(np.uint8)
 
     def display_image(self, image, image_name: str):
         if image is None:
-            logging.warning("No image to show.")
+            logging.warning("No image to show :(")
             return
 
-        cv.imshow(image_name, image)
+        # Invert image to display 0 as white and 255 as black
+        inverted_image = 255 - image
+
+        cv.imshow(image_name, inverted_image)
         key = cv.waitKey(0)
         cv.destroyAllWindows()
 
@@ -88,17 +56,101 @@ class EdgeDetection:
         kernel /= np.sum(kernel)  # Normalize
         return kernel
 
+    # Trivial way to do convolution
+    def convolution_trivial_way(self, image, kernel):
+        kernel_height, kernel_width = kernel.shape
+        image_height, image_width = image.shape
+        
+        kh = kernel_height // 2
+        kw = kernel_width // 2
+
+        kernel = np.flipud(kernel)
+        kernel = np.fliplr(kernel)
+
+        output = np.zeros((image_height, image_width))
+
+        # Start from kh, kw and subtract them to skip the borders
+        for ix in range(kh, image_height-kh):
+            for iy in range(kw, image_width-kw):
+                sum = 0
+                for kx in range(kernel_height):
+                    for kw in range(kernel_width):
+                        sum += image[ix + kx - kh, iy + kw - kw] * kernel[kx, kw]
+                output[ix, iy] = sum
+
+        return output
+
+    def convolution(self, image, kernel, is_kernel_mode=False):
+        if image is None or kernel is None:
+            logging.warning("No input for convolution")
+            return None
+
+        image_height, image_width = image.shape
+        kernel_height, kernel_width = kernel.shape
+
+        kh = kernel_height // 2
+        kw = kernel_width // 2
+
+        # Flip the kernel for correct convolution 
+        kernel = np.flipud(kernel)
+        kernel = np.fliplr(kernel)
+
+        if is_kernel_mode:
+            # Full convolution (used for kernel * kernel)
+            output_height = image_height + kernel_height - 1
+            output_width = image_width + kernel_width - 1
+            output = np.zeros((output_height, output_width), dtype=np.float32)
+
+            # Pad iamge to cover all kernel positions 
+            image = np.pad(image, ((kernel_height - 1, kernel_height - 1), (kernel_width - 1, kernel_width - 1)), mode='constant', constant_values=0)
+
+            for ix in range(output_height):
+                for iy in range(output_width):
+                    # Extract the region of the image under the kernel 
+                    # (extract the # of rows & columns centered at (ix, iy), extending by half the kernel size )
+                    region = image[ix:ix + kernel_height, iy:iy + kernel_width]
+                    output[ix, iy] = np.sum(region * kernel)
+        else:
+           output_height = image_height - kernel_height + 1
+           output_width = image_width - kernel_width + 1
+           output = np.zeros((output_height, output_width), dtype=np.float32)
+
+           for ix in range(output_height):
+            for iy in range(output_width):
+                # Extract the region of the image fully under the kernel
+                    region = image[ix:ix + kernel_height, iy:iy + kernel_width]
+                    output[ix, iy] = np.sum(region * kernel)
+
+        return output
+
+    def sobel_edge_detection(self, threshold):
+        sobel_image = self.convolution(self.image, self.sobel_kernel)
+        thresholded = self.threshold(sobel_image, threshold)
+        self.display_image(thresholded, "Sobel Edge Detection")
+
+    def sobel_edge_detection_with_smoothing(self, threshold):
+        # smoothed_image = self.convolution(self.image, self.gaussian_kernel)
+        # sobel_edge_detection = self.convolution(smoothed_image, self.sobel_kernel)
+        # thresholded_image = self.threshold(sobel_edge_detection, threshold)
+        # self.display_image(thresholded_image, "Smoothed Sobel Edge Detection")
+        
+        # Combine the two kernels
+        combined_kernels = self.convolution(self.gaussian_kernel, self.sobel_kernel, True)
+        sobel_edge_detection = self.convolution(self.image, combined_kernels)
+        thresholded_image = self.threshold(sobel_edge_detection, threshold)
+        self.display_image(thresholded_image, "Smoothed Sobel Edge Detection")
+
     def canny_edge_detection(self,lower_threshold, upper_threshold ):
         canny_edge_detection = cv.Canny(self.image, lower_threshold, upper_threshold)
         edge_detection.display_image(canny_edge_detection, "Canny Edge Detection")
 
 
 if __name__ == "__main__":
-    image = "./assets/flower.png"
-    threshold = 35
+    # image = "./assets/flower.png"
+    # threshold = 25
 
-    # image = "./assets/hand_ct.jpeg"
-    # threshold = 30
+    image = "./assets/hand_ct.jpeg"
+    threshold = 35
     
     lower_threshold = 20
     upper_threshold = 60
